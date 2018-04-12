a9db4d1f95121ff679138c8bc7faa1f75aa072a3
king4sam <king4sam990816@gmail.com>
2018-04-13 00:28:08 +0800

    inputtext value cleanup

--- script/myscript.js
+++ script/myscript.js
@@ -1,6 +1,10 @@
 'use strict';
 document.addEventListener("keydown",keydowne);
 
+function cleartext(){
+	$('input[type=text]').each(function(a,e){e.value = ''});
+}
+
 function keydowne(event){
 	var x = event.which || event.keyCode;
 	switch(x){
@@ -11,52 +15,68 @@ function keydowne(event){
 				event.stopPropagation();
 				alert('closemeplease');
 				$('#closemeplease')[0].click();
+				cleartext();
 			}
 			//close print
 			else if($('#closeHoldSlipPrint')[0] !== undefined ){
 				event.preventDefault();
+				event.stopPropagation();
 				$('#closeHoldSlipPrint')[0].click();
+				cleartext();
 			}
 
 			//attached fined
 			else if($('#content-buttons a')[1] !== undefined){
 				event.preventDefault();
+				event.stopPropagation();
 				$('#content-buttons a')[1].click();
+				cleartext();
 			}
 			//preserved
 			else if($('#HoldsListDialog_content a')[0] !== undefined){
 				event.preventDefault();
+				event.stopPropagation();
 				$('#HoldsListDialog_content a')[0].click();
+				cleartext();
 			}
 			//origin
 			else if($('#TinreadMessageDialog_content a')[0] !== undefined){
 				event.preventDefault();
+				event.stopPropagation();
 				$('#TinreadMessageDialog_content a')[0].click();
+				cleartext();
 			}
 			break;
 		case 89://y
 			//attached fined
 			if($('#content-buttons a')[0] !== undefined){
 				event.preventDefault();
+				event.stopPropagation();
 				$('#content-buttons a')[0].click();
 				document.getElementById("itemNumberField").value = '';
+				cleartext();
 			}
 			break;
 		case 80://p
 			if($('#HoldSlipDialog_content a')[0] !== undefined){
 				event.preventDefault();
+				event.stopPropagation();
 				$('#HoldSlipDialog_content a')[0].click();
 				document.getElementById("itemNumberField").value = '';
+				cleartext();
 			}
 			break;
 		case 221://]
 			if(document.getElementById("cardNumberField") ){
 				event.preventDefault();
+				event.stopPropagation();
 				document.getElementById("cardNumberField").focus();
+				cleartext();
 			}
 			break;
 		case 222://'
 			event.preventDefault();
+				event.stopPropagation();
 			window.location = '/toread/circulation/pages/loan_desk';
 			break;
 	}
