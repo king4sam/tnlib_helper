51904df6bb2d064b19da21dd3d4581f88df75edc
king4sam <king4sam990816@gmail.com>
2018-04-14 20:57:05 +0800

    TransitItemsToBeReceived popup close button

--- script/background.js
+++ script/background.js
@@ -8,4 +8,4 @@
       actions: [new chrome.declarativeContent.ShowPageAction()]
     }]);
   });
-});
+
--- script/myscript.js
+++ script/myscript.js
@@ -1,10 +1,6 @@
 'use strict';
 document.addEventListener("keydown",keydowne,true);
 
-function cleartext(){
-	$('input[type=text]').each(function(a,e){e.value = ''});
-}
-
 function keydowne(event){
 	var x = event.which || event.keyCode;
 	switch(x){
@@ -12,71 +8,59 @@ function keydowne(event){
 			//TransitItemsToBeReceived
 			if($('#closemeplease')[0] !== undefined){
 				event.preventDefault();
-				// event.stopPropagation();
 				alert('closemeplease');
 				$('#closemeplease')[0].click();
-				// cleartext();
 			}
 			//close print
 			else if($('#closeHoldSlipPrint')[0] !== undefined ){
 				event.preventDefault();
-				// event.stopPropagation();
 				$('#closeHoldSlipPrint')[0].click();
-				// cleartext();
 			}
 
 			//attached fined
 			else if($('#content-buttons a')[1] !== undefined){
 				event.preventDefault();
-				// event.stopPropagation();
 				$('#content-buttons a')[1].click();
-				// cleartext();
 			}
 			//preserved
 			else if($('#HoldsListDialog_content a')[0] !== undefined){
 				event.preventDefault();
-				// event.stopPropagation();
 				$('#HoldsListDialog_content a')[0].click();
-				// cleartext();
 			}
 			//origin
 			else if($('#TinreadMessageDialog_content a')[0] !== undefined){
 				event.preventDefault();
-				// event.stopPropagation();
 				$('#TinreadMessageDialog_content a')[0].click();
-				// cleartext();
 			}
 			break;
 		case 89://y
 			//attached fined
 			if($('#content-buttons a')[0] !== undefined){
 				event.preventDefault();
-				// event.stopPropagation();
 				$('#content-buttons a')[0].click();
 				document.getElementById("itemNumberField").value = '';
-				// cleartext();
 			}
 			break;
 		case 80://p
 			if($('#HoldSlipDialog_content a')[0] !== undefined){
 				event.preventDefault();
-				// event.stopPropagation();
 				$('#HoldSlipDialog_content a')[0].click();
 				document.getElementById("itemNumberField").value = '';
-				// cleartext();
+			}
+			break;
+		case 88://x
+			if($('#closePopupTop')[0] !== undefined){
+				$('#closePopupTop')[0].click();
 			}
 			break;
 		case 221://]
 			if(document.getElementById("cardNumberField") ){
 				event.preventDefault();
-				// event.stopPropagation();
 				document.getElementById("cardNumberField").focus();
-				// cleartext();
 			}
 			break;
 		case 222://'
 			event.preventDefault();
-				// event.stopPropagation();
 			window.location = '/toread/circulation/pages/loan_desk';
 			break;
 	}
@@ -84,13 +68,14 @@ function keydowne(event){
 
 //add close button to 
 //TransitItemsToBeReceived popup windows
-var patt = new RegExp('http:\/\/163\.26\.71\.107\/toread\/circulation\/exttransit\/TransitItemsToBeReceived,popupComponent.*');
-console.log('whether url match?' + patt.test(location.href));
-if(patt.test(location.href)){
-	var t = document.createElement('a');
-	t.id = 'closemeplease';
-	t.classList.add("dialog")
-	t.appendChild(document.createTextNode("close"));
-	t.onclick = function(e){window.close()};
-	$('table tr td').append(t);
-}
+(function(document){
+	var TransitItemsToBeReceived = new RegExp('http:\/\/163\.26\.71\.107\/toread\/circulation\/exttransit\/TransitItemsToBeReceived,popupComponent.*');
+	if(TransitItemsToBeReceived.test(location.href)){
+		var closebtn = document.createElement('a');
+		closebtn.id = 'closemeplease';
+		closebtn.classList.add("dialog")
+		closebtn.appendChild(document.createTextNode("close"));
+		closebtn.onclick = function(e){window.close()};
+		$('table tr td').append(closebtn);
+	}
+}(document));
