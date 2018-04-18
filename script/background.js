ef3df09d08f370ef15eaaf95e7161842397f4d74
king4sam <king4sam990816@gmail.com>
2018-04-18 18:29:17 +0800

    enable pageaction on anding-lib page

--- script/background.js
+++ script/background.js
@@ -4,6 +4,9 @@
     chrome.declarativeContent.onPageChanged.addRules([{
       conditions: [new chrome.declarativeContent.PageStateMatcher({
         pageUrl: {hostEquals: '163.26.71.107'}
+      }),
+      new chrome.declarativeContent.PageStateMatcher({
+        pageUrl: {hostEquals: 'anding-lib.tainan.gov.tw'}
       })],
       actions: [new chrome.declarativeContent.ShowPageAction()]
     }]);
