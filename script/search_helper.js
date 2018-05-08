2996196275a5a34c2f83e7fe896b794ad941571e
king4sam <king4sam990816@gmail.com>
2018-05-08 20:58:29 +0800

    normal book and ED book
    seperate function into normal case and ED book

--- script/search_helper.js
+++ script/search_helper.js
@@ -1,4 +1,4 @@
-var get_bookpage = function(book) {
+var get_EDbookpage = function(book) {
   const host = 'http:\/\/163.26.71.106\/';
   const webpac = 'webpac\/';
   const search = 'webpac\/search.cfm'
@@ -19,7 +19,7 @@ var get_bookpage = function(book) {
 				resolve(res.target);
 			}
 			else{
-			    resolve(get_bookpage(book))
+			    resolve(get_EDbookpage(book))
 			}
 		}
 		else {
@@ -59,13 +59,90 @@ var get_bookpage = function(book) {
 		}
 		else{
 			console.log('nolocation');
-		    resolve(get_bookpage(res.target.book))
+		    resolve(get_EDbookpage(res.target.book))
 		}
 		}
 		else {
 		// Otherwise reject with the status text
 		// which will hopefully be a meaningful error
 		reject(Error('Network Error'));
+		}
+  	}
+
+    var search_response_handler = function(res){
+		// This is called even on 404 etc
+		// so check the status
+		var parser = new DOMParser();
+		var htmlDoc =parser.parseFromString(res.target.response, "text/html");
+		var autolink = htmlDoc.getElementById('autolink');
+		if (res.target.status == 200 ) {
+		// Resolve the promise with the response text
+		if(autolink !== null){
+			// resolve(req);
+			var req= new XMLHttpRequest();
+		    req.open('GET', host +webpac + autolink.getAttribute('href'));
+		    req.setRequestHeader('Accept','text/html');
+		    req.setRequestHeader('Access-Control-Allow-Origin',searchengin_host);
+		    req.book =  res.target.book;
+			req.onload = autolink_response_handler;
+
+			// Handle network errors
+			req.onerror = function() {
+			  reject(Error("Network Error"));
+			};
+
+			// Make the request
+			req.send();
+		}
+		else{
+			console.log('noautolink');
+			resolve(get_EDbookpage(res.target.book));
+		}
+
+		}
+		else {
+		// Otherwise reject with the status text
+		// which will hopefully be a meaningful error
+		reject(Error('Network Error'));
+		}
+	}
+    // Do the usual XHR stuff
+    var req = new XMLHttpRequest();
+    req.book = book;
+    req.open('GET', host + search + par1 + book[0] + par2);
+    req.setRequestHeader('Accept','	text/html');
+    req.setRequestHeader('Access-Control-Allow-Origin',searchengin_host);
+
+    req.onload = search_response_handler;
+
+    // Handle network errors
+    req.onerror = function() {
+      reject(Error("Network Error"));
+    };
+
+    // Make the request
+    req.send();
+  });
+}
+
+var get_bookpage = function(book) {
+  const host = 'http:\/\/163.26.71.106\/';
+  const webpac = 'webpac\/';
+  const search = 'webpac\/search.cfm'
+  const par1 = '?m=ss&k0=';
+  const par2 = '&t0=k&c0=and';
+  const searchengin_host = 'http://163.26.71.106';
+  // Return a new promise.
+  return new Promise(function(resolve, reject) {
+
+  	var autolink_response_handler = function(res){
+		if (res.target.status == 200 ) {
+			resolve(res.target);
+		}
+		else {
+		// Otherwise reject with the status text
+		// which will hopefully be a meaningful error
+			reject(Error('Network Error'));
 		}
   	}
 
