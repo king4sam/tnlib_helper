094b9834b418fae89c5ee9a03d4feed92a4379df
king4sam <king4sam990816@gmail.com>
2018-05-15 11:53:08 +0800

    classify book after got results

--- script/hightlight.js
+++ script/hightlight.js
@@ -4,6 +4,7 @@ chrome.storage.local.get("reservedbooks", function(items) {
 	} else {
 
 		function reqListener(req) {
+			let index = req.target.index;
 			var regex = /(條碼：(.+))，/g;
 			const BarcodeGroupNumber = 2;
 			const IndexofCheckcol = 7;
@@ -21,51 +22,49 @@ chrome.storage.local.get("reservedbooks", function(items) {
 				//if the books was arrived, hightlight the row in table
 				barcodes.push(matches[BarcodeGroupNumber])
 			}
-
 			var bookrequests= barcodes.map(function(barcode){
-				if (anding.test(barcode)) {
-					return get_bookpage([barcode])
-						.then(function(page) {
-							var htmlDoc = new DOMParser().parseFromString(page.response, "text/html");
-							var tr = Array.apply(null, htmlDoc.querySelectorAll('#rdk_content_1 >table > tbody > tr'))
-							.filter(function(e) {
-								return e.children[0].innerText.trim() == page.book[0] 
-							});
-							var bookname = htmlDoc.getElementsByTagName('h2')[1].innerText.slice(0, -1).trim();
-							if (tr.length !== 0 && tr[0].children[statuscol].innerText.trim() == '在架') {
-								popover_content_ary.push({ 'bookname': bookname, 'barcode': barcode, 'status': '在架' });
-							}
-						},(err) => {
-							console.log('catch : ' + err);
-						});
-				}else{
-					return get_bookpage([barcode])
-						.then(function(page) {
-							var htmlDoc = new DOMParser().parseFromString(page.response, "text/html");
-							var tr = Array.apply(null, htmlDoc.querySelectorAll('#rdk_content_1 >table > tbody > tr'))
-							.filter(function(e) {
-								return e.children[0].innerText.trim() == page.book[0] 
-							});
-							var bookname = htmlDoc.getElementsByTagName('h2')[1].innerText.slice(0, -1).trim();
-							if (tr.length !== 0 && tr[0].children[statuscol].innerText.trim() == '預約保留') {
-								popover_content_ary.push({ 'bookname': bookname, 'barcode': barcode, 'status': '預約保留' });
-							}
-							else {
-								popover_content_ary.push({ 'bookname': bookname, 'barcode': barcode, 'status': '未到' });	
-							}
-						});
-				}
+				return get_bookpage([barcode]);
 			});
 			Promise.all(bookrequests)
-			.then(function(){
+			.then(function(ress){
+				var popover_content_ary = [];
+				console.log(ress);
+				var found_pages = ress.filter(p=>{return p !== 'booknotfound' && p !== 'errpage'});
+				console.log(found_pages);
+				found_pages.forEach(e=>{
+					console.log(e);
+					var barcode = e.book[0];
+					var htmlDoc = new DOMParser().parseFromString(e.response, "text/html");
+					var tr = Array.apply(null, htmlDoc.querySelectorAll('#rdk_content_1 >table > tbody > tr'))
+					.filter(function(tr) {
+						return tr.children[0].innerText.trim() == e.book[0] 
+					});
+					var bookname = htmlDoc.getElementsByTagName('h2')[1].innerText.slice(0, -1).trim();
+					if (anding.test(barcode)){
+						if (tr.length !== 0 && tr[0].children[statuscol].innerText.trim() == '在架') {
+							popover_content_ary.push({ 'bookname': bookname, 'barcode': barcode, 'status': '在架' });
+						}
+					}
+					else{
+						if (tr.length !== 0 && tr[0].children[statuscol].innerText.trim() == '預約保留') {
+							popover_content_ary.push({ 'bookname': bookname, 'barcode': barcode, 'status': '預約保留' });
+						}
+						else {
+							popover_content_ary.push({ 'bookname': bookname, 'barcode': barcode, 'status': '未到' });	
+						}
+					}
+				})
+				return popover_content_ary;
+			})
+			.then(function(popover_content_ary){
 				if (popover_content_ary.filter(function(e){
 					return e['status'] !== '未到';
 				}).length > 0) {
 					console.log(trs);
-					console.log(req.target.index);
-					trs[req.target.index].children[IndexofCheckcol-1].style.backgroundColor = '#fbff0075';
+					console.log(index);
+					trs[index].children[IndexofCheckcol-1].style.backgroundColor = '#fbff0075';
 				}
-				var tdele = trs[req.target.index].children[IndexofCheckcol-1];
+				var tdele = trs[index].children[IndexofCheckcol-1];
 				$(tdele).popover({
 					toggle: 'popover',
 					html: true,
