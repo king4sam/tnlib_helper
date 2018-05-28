b575ea77f7b475ccab0de7e9cf2c57ae902f4d0f
king4sam <king4sam990816@gmail.com>
2018-05-29 00:45:52 +0800

    msg listener
    request.query ==  "reload"

--- script/myscript.js
+++ script/myscript.js
@@ -1,9 +1,8 @@
 'use strict';
 
-var host = 'http:\/\/163\.26\.71\.107\/toread\/';
+var host = 'http:\/\/163\.26\.71\.106\/toread\/';
 
 (function(document) {
-	let seletcObserver;
 	var manage_in_batch = new RegExp(host + 'internaltranzit\/manage_in_batch*');
 	if (manage_in_batch.test(location.href)) {
 		chrome.storage.local.get("selects", function(results) {
@@ -17,60 +16,21 @@ var host = 'http:\/\/163\.26\.71\.107\/toread\/';
 				console.log('release');
 			}
 		});
-
-		chrome.runtime.onMessage.addListener(
-			function(request, sender, sendResponse) {
-				console.log(sender.tab ?
-					"from a content script:" + sender.tab.url :
-					"from the extension");
-				var setselect = function(cursta, proper) {
-					return function(records) {
-						$('#itemCurrentStatusSelection')[0].value = cursta;
-						$('#PropertySelection_0')[0].value = proper;
-					}
-				};
-
-				var obtarget = document.getElementById('HiddenBrowse');
-				if (request.query == "selects") {
-					console.log("selects");
-					var arys = []
-					arys.push(document.getElementById("itemCurrentStatusSelection").outerHTML);
-					arys.push(document.getElementById("PropertySelection_0").outerHTML);
-					sendResponse({ selects: arys });
-				} else if (request.query == "lock") {
-					if (seletcObserver !== undefined)
-						seletcObserver.disconnect();
-					seletcObserver = new MutationObserver(
-						setselect(request.setting.itemCurrentStatusSelection, request.setting.PropertySelection_0)
-					)
-					document.getElementById("PropertySelection_0").value = request.setting.PropertySelection_0;
-					document.getElementById("itemCurrentStatusSelection").value = request.setting.itemCurrentStatusSelection;
-					seletcObserver.observe(obtarget, { 'childList': true });
-					console.log("lock");
-					console.log(request.setting);
-					sendResponse({ status: "OK" });
-				} else if (request.query == "release") {
-					if (seletcObserver !== undefined)
-						seletcObserver.disconnect();
-					document.getElementById("PropertySelection_0").value = 0;
-					document.getElementById("itemCurrentStatusSelection").value = 0;
-					console.log("release");
-					sendResponse({ status: "OK" });
-				}
-			}
-		);
 	}
 }(document));
 
 (function(document) {
 	chrome.storage.local.get("hotkeys", function(results) {
 		var namecodemap;
-		if (chrome.runtime.lastError || undefined === results['hotkeys']) {} else {
+		if (chrome.runtime.lastError || undefined === results['hotkeys']) {
+		} else {
 			namecodemap = results['hotkeys'];
 		}
 		$(document).keypress(keydowne);
 
 		function keydowne(event) {
+			console.log(event.which);
+			console.log(event.keyCode);
 			var x = event.which;
 			if (x >= 65 && x <= 90) {
 				x += 32;
@@ -101,6 +61,10 @@ var host = 'http:\/\/163\.26\.71\.107\/toread\/';
 				else if ($('#TinreadMessageDialog_content a')[0] !== undefined) {
 					event.preventDefault();
 					$('#TinreadMessageDialog_content a')[0].click();
+				}
+				else if($('#exceptionDialogHandle')[0] !== undefined){
+					event.preventDefault();
+					$('#exceptionDialogHandle')[0].click();
 				}
 			}
 			var yesaction = function() {
@@ -144,24 +108,78 @@ var host = 'http:\/\/163\.26\.71\.107\/toread\/';
 			}
 
 			var keyconbinations = [
-				{ name: "取消、關閉、否", action: closeaction },
-				{ name: "是、確定", action: yesaction },
-				{ name: "列印", action: printaction },
-				{ name: "聚焦證號欄", action: cardNumberFieldaction },
-				{ name: "借還書作業", action: loan_deskaction },
-				{ name: "移轉寄送", action: TransitItemsToBesendaction },
+				{ name: "取消、關閉、否", action: closeaction},
+				{ name: "是、確定", action: yesaction},
+				{ name: "列印", action: printaction},
+				{ name: "聚焦證號欄", action: cardNumberFieldaction},
+				{ name: "借還書作業", action: loan_deskaction},
+				{ name: "移轉寄送", action: TransitItemsToBesendaction},
+				{ name: "關閉", action: managecloseaction }
 			]
 			var targetname = namecodemap.find(e => { return e.code === x });
-			console.log(targetname);
 			if (targetname) {
+				console.log(targetname.name);
 				var targetaction = keyconbinations.find(e => { return e.name === targetname.name });
 				if (targetaction) {
+					console.log(targetaction);
 					targetaction.action();
 				}
 			}
 		}
 	});
 }(document));
+
+chrome.runtime.onMessage.addListener(
+	function(request, sender, sendResponse) {
+		console.log(sender.tab ?
+			"from a content script:" + sender.tab.url :
+			"from the extension");
+
+		var manage_in_batch = new RegExp(host + 'internaltranzit\/manage_in_batch*');
+		if (manage_in_batch.test(location.href)) {
+			let seletcObserver;
+			var setselect = function(cursta, proper) {
+				return function(records) {
+					$('#itemCurrentStatusSelection')[0].value = cursta;
+					$('#PropertySelection_0')[0].value = proper;
+				}
+			};
+
+			var obtarget = document.getElementById('HiddenBrowse');
+			if (request.query == "selects") {
+				console.log("selects");
+				var arys = []
+				arys.push(document.getElementById("itemCurrentStatusSelection").outerHTML);
+				arys.push(document.getElementById("PropertySelection_0").outerHTML);
+				sendResponse({ selects: arys });
+			} else if (request.query == "lock") {
+				if (seletcObserver !== undefined)
+					seletcObserver.disconnect();
+				seletcObserver = new MutationObserver(
+					setselect(request.setting.itemCurrentStatusSelection, request.setting.PropertySelection_0)
+				)
+				document.getElementById("PropertySelection_0").value = request.setting.PropertySelection_0;
+				document.getElementById("itemCurrentStatusSelection").value = request.setting.itemCurrentStatusSelection;
+				seletcObserver.observe(obtarget, { 'childList': true });
+				console.log("lock");
+				console.log(request.setting);
+				sendResponse({ status: "OK" });
+			} else if (request.query == "release") {
+				if (seletcObserver !== undefined)
+					seletcObserver.disconnect();
+				document.getElementById("PropertySelection_0").value = 0;
+				document.getElementById("itemCurrentStatusSelection").value = 0;
+				console.log("release");
+				sendResponse({ status: "OK" });
+			} 
+		}
+
+		if(request.query ==  "reload"){
+			console.log("reload");
+			location.reload();
+		}
+	}	
+);
 
 //add close button to 
 //TransitItemsToBeReceived popup windows
